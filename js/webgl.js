6a648aadd299408cb3cb4b1225f0a585b5e2bbe1
Marta <mchupil@mail.ru>
2018-02-06 00:20:26 +0200

    some changed

--- js/webgl.js
+++ js/webgl.js
@@ -15,3 +15,19 @@ let initWebGL = () => {
           console.warn('Error with loading ' + error);
       }) 
 }
+
+let startWebGL = (vertexShaderText, fragmentShaderText) => {
+    let canvas = document.getElementById('webgl-canvas');
+    let gl = canvas.getContext('webgl');
+
+    if (!gl) {
+        console.warn('No Support WebGL');
+        return;
+    }
+
+    console.log(gl);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    initWebGL();
+});
